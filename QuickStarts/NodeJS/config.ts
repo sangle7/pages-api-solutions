af6bcf6b12c35e330267d53d5466bb96f21e4d36
Sangle <whb97@163.com>
2022-12-07 13:04:36 +0800

    Update config.ts

--- QuickStarts/NodeJS/config.ts
+++ QuickStarts/NodeJS/config.ts
@@ -6,10 +6,10 @@ export interface IConfig {
 }
 
 const config: IConfig = {
-  appId: 'ca4177e3-6512-48d2-b003-98d7a79ed4d6',
-  appSecret: 'Ob18Q~wz3-YAnMsHiyiBfU13hAHK~iy_ccp~ddfX',
-  tenantId: 'ef379d32-30d7-46fd-9bc8-971e0cfff9bd',
-  siteId: '80c0b48a-973d-4586-9c73-ecd5cecdd0e6',
+  appId: '<enter app/client Id>',
+  appSecret: '<enter app/client secret>',
+  tenantId: '<enter tenant Id>',
+  siteId: '<enter site Id>',
 };
 
 export default config;
